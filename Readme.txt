47f5537eb76d58bdbb6c94297e104a58967413da
simranbijay786 <84765466+simranbijay786@users.noreply.github.com>
2021-05-25 13:51:24 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,15 @@
+Great Learning WebPage Assignment
+This repository houses the assets used to build the Great Learning Webpage.
+Prerequisites
+The website is built and developed using the Sublime which provides immense features including ,Goto Anything, quick navigation to files, symbols, or lines,command palette uses adaptive matching for quick keyboard invocation of arbitrary commands and also helps in simultaneous editing.Also, git is used  that will be  managing the source code.
+
+Setup
+
+If you installed git you can clone the code to your machine, or download a ZIP of all the files directly.
+Download the ZIP from this location, or run the following git command to clone the files to your machine.
+git clone 
+Once the files are on your machine, open the GreatLearning folder in Sublime/Notepad.
+
+Website Content
+
+The content for the Great Learning Webpage is in /GreatLearning while the documentation is in Readme.md.
---
+++ jsfile.js
@@ -0,0 +1,56 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Carousel");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  // setTimeout(showSlides, 2000); 
+}
+
+
+
+function myFunction() {
+  var x = document.getElementById("myEmail").pattern;
+  document.getElementById("demo").innerHTML = x;
+}
+function myFunction() {
+  var x = document.getElementById("num").pattern;
+  document.getElementById("demo").innerHTML = x;
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
